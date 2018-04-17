6085a48f743562eba533f1f0227dc4f201298d37
Dimitar Kokotanekov <acid901@mail.bg>
2018-04-17 23:14:26 +0300

    upload img to canvas

---
+++ main.js
@@ -0,0 +1,46 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+let img = new Image();
+let fileName = '';
+
+const dwnBtn = document.getElementById('download-btn');
+const uplFile = document.getElementById('upload-file');
+const revertBtn = document.getElementById('revert-btn');
+
+
+// Upload File
+uplFile.addEventListener("change", () => {
+  // Get File
+  const file = document.getElementById("upload-file").files[0];
+  // Init FileReader API
+  const reader = new FileReader();
+
+  // Check for file
+  if (file) {
+    // Set file name
+    fileName = file.name;
+    // Read data as URL
+    reader.readAsDataURL(file);
+  }
+
+  // Add image to canvas
+  reader.addEventListener(
+    "load",
+    () => {
+      // Create image
+      img = new Image();
+      // Set image src
+      img.src = reader.result;
+      // On image load add to canvas
+      img.onload = function() {
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.drawImage(img, 0, 0, img.width, img.height);
+        canvas.removeAttribute("data-caman-id");
+      };
+    },
+    false
+  );
+});
+
